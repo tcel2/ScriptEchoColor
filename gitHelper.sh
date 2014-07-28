fc9ac2aee49ae5731b9922804d159017afd6ea45
teike <teike@git.code.sf.net>
2014-07-28 19:26:36 -0300

    .

--- gitHelper.sh
+++ gitHelper.sh
@@ -28,7 +28,7 @@ SECFUNCuniqueLock --daemonwait
 
 while true;do
 	echoc --info "Git helper (hit ctrl+c to exit)"
-	echoc -Q "git@O_commitWithGitGui/_diffLastTagFromMaster/_pushTagsToRemote/_browseWithGitk";
+	echoc -Q "git@O_commitWithGitGui/_diffLastTagFromMaster/_pushTagsToRemote/_browseWithGitk"&&:
 	case "`secascii $?`" in 
 		c) echoc -x "git gui";; 
 		b) echoc -x "gitk";; 
