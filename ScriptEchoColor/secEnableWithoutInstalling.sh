25b5d4f1eaae83176a11a209eaed5e48bffb74a7
teike <teike@git.code.sf.net>
2014-11-24 15:25:19 -0200

    secEnableWithoutInstalling.sh

---
+++ ScriptEchoColor/secEnableWithoutInstalling.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Copyright (C) 2004-2014 by Henrique Abdalla
+#
+# This file is part of ScriptEchoColor.
+#
+# ScriptEchoColor simplifies Linux terminal text colorizing, formatting 
+# and several steps of script coding.
+#
+# ScriptEchoColor is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# ScriptEchoColor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ScriptEchoColor. If not, see <http://www.gnu.org/licenses/>.
+#
+# Homepage: http://scriptechocolor.sourceforge.net/
+# Project Homepage: https://sourceforge.net/projects/scriptechocolor/
+
+echo
+
+echo ">>---> update from git... <---<<"
+git pull origin
+echo
+
+cd bin
+strSECbinInstPathGit="`pwd`"
+ln -vs ../bin.extras/*   ./ 2>&1 |egrep -v ": File exists$"
+ln -vs ../bin.examples/* ./ 2>&1 |egrep -v ": File exists$"
+echo
+
+echo ">>---> initialize ScriptEchoColor <---<<"
+./echoc --info "ScriptEchoColor @nenabled!" #will initialize it
+echo
+
+echo ">>---> now add this '$strSECbinInstPathGit' to your PATH variable <---<<"
+echo
