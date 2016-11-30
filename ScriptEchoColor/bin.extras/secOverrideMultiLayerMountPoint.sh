c075ac47f0a243a297b791ebaffba2be7e5e91fa
teike <teike@git.code.sf.net>
2016-11-29 22:31:10 -0200

    .

--- ScriptEchoColor/bin.extras/secOverrideMultiLayerMountPoint.sh
+++ ScriptEchoColor/bin.extras/secOverrideMultiLayerMountPoint.sh
@@ -24,33 +24,87 @@
 
 eval `secinit`
 
-strMountAt="$1"
+: ${strEnvVarUserCanModify:="test"}
+export strEnvVarUserCanModify #help this variable will be accepted if modified by user before calling this script
+export strEnvVarUserCanModify2 #help test
+strExample="DefaultValue"
+CFGstrTest="Test"
+astrRemainingParams=()
+astrAllParams=("${@-}") # this may be useful
+bUmount=false
+SECFUNCcfgReadDB #after default variables value setup above
+while ! ${1+false} && [[ "${1:0:1}" == "-" ]];do # checks if param is set
+	SECFUNCsingleLetterOptionsA;
+	if [[ "$1" == "--help" ]];then #help show this help
+		SECFUNCshowHelp --colorize "\t[strMountAt]"
+		SECFUNCshowHelp --colorize "\tConfig file: '`SECFUNCcfgFileName --get`'"
+		echo
+		SECFUNCshowHelp
+		exit 0
+	elif [[ "$1" == "-u" ]];then #help unmount and exit
+		bUmount=true;
+	elif [[ "$1" == "--exampleoption" || "$1" == "-e" ]];then #help <strExample> MISSING DESCRIPTION
+		shift
+		strExample="${1-}"
+	elif [[ "$1" == "--cfg" ]];then #help <strCfgVarVal>... Configure and store a variable at the configuration file with SECFUNCcfgWriteVar, and exit. Use "help" as param to show all vars related info. Usage ex.: CFGstrTest="a b c" CFGnTst=123 help
+		shift
+		pSECFUNCcfgOptSet "$@";exit 0;
+	elif [[ "$1" == "--" ]];then #help params after this are ignored as being these options, and stored at astrRemainingParams
+		shift #astrRemainingParams=("$@")
+		while ! ${1+false};do	# checks if param is set
+			astrRemainingParams+=("$1")
+			shift #will consume all remaining params
+		done
+	else
+		echoc -p "invalid option '$1'"
+		#"$SECstrScriptSelfName" --help
+		$0 --help #$0 considers ./, works best anyway..
+		exit 1
+	fi
+	shift&&:
+done
+# IMPORTANT validate CFG vars here before writing them all...
+SECFUNCcfgAutoWriteAllVars #this will also show all config vars
+
+strMountAt="${1-}"
+
+if [[ -z "$strMountAt" ]];then
+	echoc -p "invalid strMountAt='$strMountAt'"
+	exit 1
+fi
 
 function FUNCumount(){
 	SECFUNCexecA -ce sudo -k umount "$strMountAt"
 	SECFUNCexecA -ce trash -v "$strMountAt"
 }
 
-if [[ -a "$strMountAt" ]];then
-	if ! [[ -d "$strMountAt" ]];then
-		echoc -p "should be a directory strMountAt='$strMountAt', or should not exist..."
-	elif mount |egrep "/${strMountAt} type aufs";then
-		if echoc -q "umount and trash it?";then
-			FUNCumount
-		fi
-	elif [[ -d "$strMountAt" ]];then
-		SECFUNCexecA -ce ls -lR "$strMountAt/"
+if $bUmount;then
+	FUNCumount
+	exit 0
+fi
+
+if mount |egrep "/${strMountAt} type aufs";then
+	echoc --info "already mounted!"
+	exit 0
+else
+	if [[ -d "$strMountAt" ]];then
 		echoc --info "the mount point strMountAt='$strMountAt' access would be overriden, and its files will not be accessible..."
-		echoc -p "strMountAt='$strMountAt' should not exist..."
+#		echoc -p "strMountAt='$strMountAt' should not exist..."
 		
 		if [[ -z "`ls -A "$strMountAt/"`" ]];then
 			echoc --info "it is empty..."
 			SECFUNCexecA -ce trash -v "$strMountAt"
+		else
+			echoc -p "not empty!"
+			SECFUNCexecA -ce ls -lR "$strMountAt/"
+			exit 1
+		fi
+	else
+		if [[ -a "$strMountAt" ]];then
+			echoc -p "should be a directory strMountAt='$strMountAt', or should not exist..."
+			exit 1
 		fi
 	fi
-	
-	echoc --info "run it again..."
-	exit 1
 fi
 
 # the layers override priority is from left (top override) to right
@@ -80,7 +134,7 @@ SECFUNCexecA -ce sudo -k mount -t aufs -o br="$strWriteLayer:$strLayerBranch" no
 
 SECFUNCexecA -ce ls -d "${strMountAt}"*
 
-echoc -w "to umount and remove"
-FUNCumount
+#echoc -w "to umount and remove"
+#FUNCumount
 
 
