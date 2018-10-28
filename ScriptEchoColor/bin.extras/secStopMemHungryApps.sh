9c3bee88daf752093b63eabf5bb0a82bbcb2d116
teike <teike@git.code.sf.net>
2018-10-28 21:54:37 -0200

    ScriptEchoColor/bin.extras/secStopMemHungryApps.sh: improved;

--- ScriptEchoColor/bin.extras/secStopMemHungryApps.sh
+++ ScriptEchoColor/bin.extras/secStopMemHungryApps.sh
@@ -26,6 +26,17 @@ source <(secinit --extras)
 
 echo "SelfPid=$$"
 
+function FUNCCHILDaddIgnorePid() { #<lnPid>
+  local lnPid="$1"
+  (
+    echo "SENDING PID TO IGNORE: $lnPid"
+    SECFUNCexecA -ce kill -SIGUSR1 $$ # will make it wait pipe be filled up
+    echo "$lnPid" >>"$strFifoFl" # will only return after it is read!
+    echo "SENT."
+  )&
+}
+
+astrRegexIgnorePgrep=(Xorg compiz metacity xfwm4 kwin mutter)
 strFifoFl="`SECFUNCcreateFIFO`"
 : ${nMemLimKB:=500000} #help
 anPidIgnore=() #TODO use cmd regex later at default config file to auto ignore pids
@@ -35,11 +46,22 @@ anPidIgnore=() #TODO use cmd regex later at default config file to auto ignore p
 bReadFIFO=false
 trap 'bReadFIFO=true' SIGUSR1
 while true;do
+  nMaxCols=`tput cols` #;declare -p nMaxCols
+#  declare -p anPidIgnore
+  
   IFS=$'\n' read -d '' -r -a astrList < <(
     ps --no-headers -A -o rss,pid,state,cmd --sort -rss \
           |head -n $nLimPids \
           |sed -r 's@([[:digit:]]*) *([[:digit:]]*) *([[:alnum:]]) *(.*)@nResKB=\1;nPid=\2;strState="\3";strCmd="\4";@'  
   )&&:
+  
+  for strRegexIgnore in "${astrRegexIgnorePgrep[@]}";do
+    if nPidRegexIgnore=`pgrep $strRegexIgnore`;then
+      if ! SECFUNCarrayContains anPidIgnore $nPidRegexIgnore;then
+        anPidIgnore+=($nPidRegexIgnore)
+      fi
+    fi
+  done
   
   bShowHeader=true
   for strLine in "${astrList[@]}";do 
@@ -48,14 +70,19 @@ while true;do
     if $bReadFIFO;then
       strFIFOdata="`cat <"$strFifoFl"`" # this will WAIT until somthing is written to the PIPE!!!
       if [[ -n "$strFIFOdata" ]];then
-        declare -p anPidIgnore
-        echo "EVAL: $strFIFOdata"
-        eval "$strFIFOdata"
+        if ps -p $strFIFOdata;then
+          nPidIgnore=$strFIFOdata
+          SECFUNCexecA -ce kill -SIGCONT $nPidIgnore
+          anPidIgnore+=($nPidIgnore)
+        fi
+        #~ declare -p anPidIgnore
+        #~ echo "EVAL: $strFIFOdata"
+        #~ eval "$strFIFOdata"
         #~ echo "$strFIFOdata" |while read strSrcExec;do
           #~ echo "EVAL: $strSrcExec"
           #~ eval "$strSrcExec"
         #~ done
-        declare -p anPidIgnore
+        #~ declare -p anPidIgnore
       fi
       bReadFIFO=false
     fi
@@ -65,7 +92,7 @@ while true;do
         SECFUNCdrawLine --left " nPid nResKB strState strCmd "
         bShowHeader=false
       fi
-      echo $nPid $nResKB $strState $strCmd |sed -r "s@(.{`tput cols`}).*@\1@"
+      echo $nPid $nResKB $strState $strCmd |sed -r "s@(.{$nMaxCols}).*@\1@"
       #~ if ! SECFUNCarrayContains anPidManaged $nPid;then
         #~ anPidManaged+=($nPid)
       #~ fi
@@ -87,19 +114,21 @@ while true;do
           strText="${astrText[*]}"
           if yad --title="$SECstrScriptSelfName" --info \
             --button="gtk-ok:0" --button="gtk-close:1" \
-            --text="$strText";
+            --text="${strText:0:255}";
           then
-            echo "SENDING PID TO IGNORE: $nPid"
-            SEC_WARN=true
-            SECFUNCexecA -ce kill -SIGUSR1 $$ # will make it wait pipe be filled up
-            echo "anPidIgnore+=($nPid);" >>"$strFifoFl" # will only return after it is read!
-            SECFUNCexecA -ce kill -SIGCONT $nPid
+            FUNCCHILDaddIgnorePid $nPid
+#            echo "SENDING PID TO IGNORE: $nPid"
+            #~ SEC_WARN=true
+ #           SECFUNCexecA -ce kill -SIGUSR1 $$ # will make it wait pipe be filled up
+  #          echo "$nPid" >>"$strFifoFl" # will only return after it is read!
+            #~ echo "anPidIgnore+=($nPid);" >>"$strFifoFl" # will only return after it is read!
+            #~ SECFUNCexecA -ce kill -SIGCONT $nPid
             #~ while ! echo "anPidIgnore+=($nPid)" >>"$strFifoFl";do
               #~ ls -l "$strFifoFl" &&:
               #~ SECFUNCechoWarnA "Failed SENDING PID TO IGNORE, retrying."
               #~ sleep 1
             #~ done
-            echo "SENT."
+   #         echo "SENT."
           fi
         )&
       fi
@@ -117,12 +146,27 @@ while true;do
       #~ --column="" --column="PID" --column="ResKB" --column="CMD" \
       #~ 0 123 321 asdf 1 124 322 asdfg
   #~ fi
-  if echoc -t 10 -q "add one PID to ignore list?";then
-    nPidIgnore=`echoc -S "what PID"`;
-    if [[ -n "$nPidIgnore" ]];then
-      anPidIgnore+=($nPidIgnore)
-      declare -p anPidIgnore
-      SECFUNCexecA -ce kill -SIGCONT $nPidIgnore
-    fi
-  fi
+  ScriptEchoColor -t 10 -Q "question@O_add one PID to ignore list/_list ignored pids"&&:; nRet=$?; case "`secascii $nRet`" in 
+    a)
+      nPidIgnore=`echoc -S "what PID"`;
+      if [[ -n "$nPidIgnore" ]];then
+        FUNCCHILDaddIgnorePid $nPidIgnore
+      fi
+      ;; 
+    l)
+      echoc --info "Ignored PIDs"
+      SECFUNCexecA -ce ps -o rss,pid,state,cmd --sort -rss -p "${anPidIgnore[@]}" |sed -r "s@(.{$nMaxCols}).*@\1@" &&:
+      ;;
+    *)if((nRet==1));then SECFUNCechoErrA "err=$nRet";exit 1;fi;; 
+  esac
+  
+#  if echoc -t 10 -q "add one PID to ignore list?";then
+    #nPidIgnore=`echoc -S "what PID"`;
+    #if [[ -n "$nPidIgnore" ]];then
+     # FUNCCHILDaddIgnorePid $nPidIgnore
+      #~ anPidIgnore+=($nPidIgnore)
+      #~ declare -p anPidIgnore
+      #~ SECFUNCexecA -ce kill -SIGCONT $nPidIgnore
+    #fi
+  #fi
 done
